dd598b45a3d243e24e34de2628a7cb729f84a762
Ben Darnell <ben@bendarnell.com>
2010-08-30 17:04:05 -0700

    First cut at a non-curl-based AsyncHTTPClient

---
+++ tornado/simple_httpclient.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+
+from cStringIO import StringIO
+from tornado.httpclient import HTTPRequest, HTTPResponse, HTTPError
+from tornado.httputil import HTTPHeaders
+from tornado.ioloop import IOLoop
+from tornado.iostream import IOStream
+from tornado import stack_context
+
+import contextlib
+import functools
+import logging
+import re
+import socket
+import urlparse
+
+class SimpleAsyncHTTPClient(object):
+    # TODO: singleton magic?
+    def __init__(self, io_loop=None):
+        self.io_loop = io_loop or IOLoop.instance()
+
+    def close(self):
+        pass
+
+    def fetch(self, request, callback, **kwargs):
+        if not isinstance(request, HTTPRequest):
+            request = HTTPRequest(url=request, **kwargs)
+        callback = stack_context.wrap(callback)
+
+        @contextlib.contextmanager
+        def cleanup():
+            try:
+                yield
+            except Exception, e:
+                logging.warning("uncaught exception", exc_info=True)
+                callback(HTTPResponse(request, 599, error=e))
+        with stack_context.StackContext(cleanup):
+            parsed = urlparse.urlsplit(request.url)
+            sock = socket.socket()
+            #sock.setblocking(False) # TODO non-blocking connect
+            sock.connect((parsed.netloc, 80))  # TODO: other ports, https
+            stream = IOStream(sock, io_loop=self.io_loop)
+            # TODO: query parameters
+            if "Host" not in request.headers:
+                request.headers["Host"] = parsed.netloc
+            request_lines = ["%s %s HTTP/1.1" % (request.method,
+                                                 parsed.path or '/')]
+            for k, v in request.headers.get_all():
+                request_lines.append("%s: %s" % (k, v))
+            if logging.getLogger().isEnabledFor(logging.DEBUG):
+                for line in request_lines:
+                    logging.debug(line)
+            stream.write("\r\n".join(request_lines) + "\r\n\r\n")
+            stream.read_until("\r\n\r\n", functools.partial(self._on_headers,
+                                                            request, callback, stream))
+
+    def _on_headers(self, request, callback, stream, data):
+        logging.debug(data)
+        first_line, _, header_data = data.partition("\r\n")
+        match = re.match("HTTP/1.[01] ([0-9]+) .*", first_line)
+        assert match
+        code = int(match.group(1))
+        headers = HTTPHeaders.parse(header_data)
+        if headers.get("Transfer-Encoding") == "chunked":
+            chunks = []
+            stream.read_until("\r\n", functools.partial(self._on_chunk_length,
+                                                        request, callback, stream, code, headers, chunks))
+        elif "Content-Length" in headers:
+            stream.read_bytes(int(headers["Content-Length"]),
+                              functools.partial(self._on_body, request, callback, stream, code, headers))
+        else:
+            raise Exception("No Content-length or chunked encoding, "
+                            "don't know how to read")
+
+    def _on_body(self, request, callback, stream, code, headers, data):
+        response = HTTPResponse(request, code, headers=headers,
+                                buffer=StringIO(data)) # TODO
+        callback(response)
+
+    def _on_chunk_length(self, request, callback, stream, code, headers, chunks, data):
+        # TODO: "chunk extensions" http://tools.ietf.org/html/rfc2616#section-3.6.1
+        length = int(data.strip(), 16)
+        if length == 0:
+            self._on_body(request, callback, stream, code, headers,
+                          ''.join(chunks))
+        else:
+            stream.read_bytes(length + 2,  # chunk ends with \r\n
+                              functools.partial(self._on_chunk_data,
+                                                request, callback, stream,
+                                                code, headers, chunks))
+
+    def _on_chunk_data(self, request, callback, stream, code, headers, chunks, data):
+        assert data[-2:] == "\r\n"
+        chunks.append(data[:-2])
+        stream.read_until("\r\n", functools.partial(self._on_chunk_length,
+                                                    request, callback, stream, code, headers, chunks))
+
+
+def main():
+    from tornado.options import define, options, parse_command_line
+    args = parse_command_line()
+    client = SimpleAsyncHTTPClient()
+    io_loop = IOLoop.instance()
+    for arg in args:
+        def callback(response):
+            io_loop.stop()
+            response.rethrow()
+            print response.body
+        client.fetch(arg, callback)
+        io_loop.start()
+
+if __name__ == "__main__":
+    main()
